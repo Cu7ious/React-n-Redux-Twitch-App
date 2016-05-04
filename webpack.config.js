30855f88ea260d98dde410c18eff611be78cffb5
Curious <garkusha.sergiy@gmail.com>
2016-05-04 15:59:03 -0700

    '

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+// const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './app/core.js'
+  ],
+
+  output: {
+    // path: __dirname + '/webpack-bundle',
+    path: './webpack-bundle',
+    filename: 'wp-bundle.js'
+  },
+
+  devServer: {
+    inline: true,
+    port: process.env.PORT
+  },
+
+  // plugins: [
+  //   new webpack.optimize.DedupePlugin(),
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     minimize: true,
+  //     compress: {
+  //       warnings: false
+  //     }
+  //   }),
+  //   new webpack.DefinePlugin({
+  //     'process.env': {
+  //       'NODE_ENV': JSON.stringify('production')
+  //     }
+  //   })
+  // ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  }
+};
