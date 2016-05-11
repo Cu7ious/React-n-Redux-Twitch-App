5731d7bb9b616362cae8c95293192c0ba84df349
Curious <garkusha.sergiy@gmail.com>
2016-05-10 22:06:40 -0700

    Allmost done :)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+// run in shell
+// NODE_ENV=production webpack -p --config webpack.config.js
+
 const webpack = require('webpack');
 
 module.exports = {
@@ -22,16 +25,16 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    // new webpack.optimize.AggressiveMergingPlugin()
-    // new webpack.optimize.OccurenceOrderPlugin(),
-    // new webpack.optimize.DedupePlugin(),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: { warnings: false },
-    //   comments: false,
-    //   // sourceMap: false,
-    //   // mangle: true,
-    //   // minimize: true
-    // })
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      comments: false,
+      // sourceMap: false,
+      mangle: true,
+      minimize: true
+    })
   ],
 
   module: {
